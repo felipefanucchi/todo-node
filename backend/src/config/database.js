08b3b89925b137529d2dfa550f2d197ed46e2cc5
Felipe Fanucchi <felipefanucchi2010@hotmail.com>
2021-04-17 14:58:50 -0300

    Creating model and migration for User

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,12 +1,7 @@
 module.exports = {
-  host: 'db',
+  host: '0.0.0.0',
   username: 'root',
   password: 'root',
   database: 'todonode',
-  dialect: 'postgres',
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
-  },
+  dialect: 'postgres'
 }
