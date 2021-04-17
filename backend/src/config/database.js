00ad6406d8cf860976fd53d7b02d92f4e4e883f5
Felipe Fanucchi <felipefanucchi2010@hotmail.com>
2021-04-17 12:14:09 -0300

    changing path of sequelize files (db, config, seeders and migrations)

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: '',
+  password: '',
+  database: '',
+  dialect: 'postgres',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
