3d9d2b5805d7b7b8e1f614cbf3a03cc514a8c462
Felipe Fanucchi <felipefanucchi2010@hotmail.com>
2021-04-18 09:09:26 -0300

    Update ports to use .env

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`🚀 Server ready at http://localhost:3000`);
+  console.log(`🚀 Server ready at http://localhost:${process.env.PORT}`);
 });
